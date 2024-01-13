0a27cca00777e480387f38e93a7f614a40162936
Jennifer <jennifertrinhly@berkeley.edu>
2024-01-12 17:13:50 -0800

    Created inputs for lab01

---
+++ lab1.py
@@ -0,0 +1,18 @@
+# lab1.py
+
+# Starter code for lab 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+# Please see the README in this repository for the requirements of this lab exercise
+
+# Stephen Huang
+# steph13@uci.edu  
+# 42399418
+
+import operator
+
+print("Hello! This is my calculator for ICS 32.\n")
+var1 = int(input("Enter your first variable: "))
+var2 = int(input("Enter your second variable: "))
+operate = input("Enter your desired operator (+, -, x, or %): ")
+
